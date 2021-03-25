9607d56aee6a228cd0c8a608ef404093be4916fc
youtingkun <youtingkun@techsailing.com>
2021-03-25 16:24:59 +0800

    init

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://www.youtingkun.com',
+  pathPrefix: '/',
+  title: 'Blog by youtingkun',
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
+  disqusShortname: 'https-youtingkun-github-io',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    // {
+    //   label: 'Contact me',
+    //   path: '/pages/contacts'
+    // }
+  ],
+  author: {
+    name: 'youtingkun',
+    photo: 'http://qiniu.youtingkun.com/image/avatar.jpg',
+    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    contacts: {
+      email: '',
+      facebook: 'profile.php?id=100015745118310',
+      telegram: '',
+      twitter: '',
+      github: 'youtingkun',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
---
+++ content/pages/about.md
@@ -0,0 +1,9 @@
+---
+title: "About me"
+template: "page"
+socialImage: "/media/image-2.jpg"
+---
+
+
+
+![Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.](/media/image-2.jpg)
